6af2ecfdde5655dd15137647d3b2099e0f85a0ca
Jürgen Weigert <jnweiger@gmail.com>
2025-04-14 18:20:01 +0200

    avoid useless mkdir calls

--- deployments/examples/bare-metal-simple/install.sh
+++ deployments/examples/bare-metal-simple/install.sh
@@ -69,11 +69,10 @@ echo "Downloading ${dlurl}/${dlfile}"
 curl -L -o "${dlfile}" --progress-bar "${dlurl}/${dlfile}"
 chmod 755 ${dlfile}
 
-mkdir data config
-
 basedir="${OC_BASE_DIR:-$(pwd)}"
 export OC_CONFIG_DIR="$basedir/config"
 export OC_BASE_DATA_PATH="$basedir/data"
+mkdir "$OC_CONFIG_DIR" "$OC_BASE_DATA_PATH"
 
 # It is bound to localhost for now to deal with non existing routes
 # to certain host names for example in WSL
